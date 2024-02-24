7c7d16308ea1cb8b7f498dea1033d5f428ff0ebc
mirajpatel752 <mirajpatel752@gmail.com>
2024-02-25 01:54:43 +0530

    button send  req  google  login

--- src/Page/About/index.js
+++ src/Page/About/index.js
@@ -1,7 +1,31 @@
+import { useGoogleLogin } from "@react-oauth/google"
+import axios from "axios"
+
 const About = () => {
+
+    const  login  = useGoogleLogin({
+        onSuccess: async(response) =>{
+            try {
+                const  res  = await axios.get("https://www.googleapis.com.oauth2/v3/userinfo",
+                {
+                    headers:{
+                        Authorization:`Bearer ${response.access_token}`
+                    }
+                }
+                )
+                console.log(res,"res")
+            } catch (error) {
+                
+            }
+        }
+        })
+    
     return (
         <>
         About
+        <button onClick={() => login()}>
+            Google  login
+        </button>
         </>
     )
 }
